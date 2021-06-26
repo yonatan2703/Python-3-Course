541469f12bead755162eedabb21908a4514168cd
Yonatan Barhen <70335092+yonatan2703@users.noreply.github.com>
2021-06-26 20:54:26 +0300

    continued the project

--- 2. RPG-Battle/main.py
+++ 2. RPG-Battle/main.py
@@ -2,14 +2,33 @@ from classes.game import Person, bcolors
 
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 60}, 
-         {"name": "Thunder", "cost": 10, "dmg": 70}, 
-         {"name": "Blizzard", "cost": 10, "dmg": 80}]
+         {"name": "Thunder", "cost": 11, "dmg": 70}, 
+         {"name": "Blizzard", "cost": 12, "dmg": 80}]
 
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
 
-print(player.generate_damage())
+running = True
+i = 0
 
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
-print(player.generate_spell_damage(2))
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
+
+while running:
+    print("=============================")
+    player.choose_action()
+    choice = input("Choose action: ")
+    index = int(choice) - 1
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+
+    enemy_choice = 1
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
+
+    # running = False
